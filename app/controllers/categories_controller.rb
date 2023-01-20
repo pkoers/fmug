27e604460db704ddbfd7229cf5f0ae6e4dbe073d
Patrick Koers <pkoers75@gmail.com>
2023-01-20 19:59:22 +0100

    adding a category is now available

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,4 +7,22 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def show
+    @categories = Category.all
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to action: "index"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end

5a9c98139b9e85e0bbac5ab10a8d1e71b6186425
Patrick Koers <pkoers75@gmail.com>
2025-02-07 22:09:08 +0100

    IDE updated 7feb25

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,7 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+Rails.application.config.assets.configure do |env|
+  env.append_path "vendor/assets"
+end

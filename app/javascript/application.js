ce1613fcc9c4f85f4b5ad7983ce6f6e3484b82de
Patrick Koers <pkoers75@gmail.com>
2025-03-24 19:52:20 +0100

    added Shoelaces

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import '../stylesheets/application.scss'
+import { setBasePath, SlAlert, SlAnimation, SlButton } from '@shoelace-style/shoelace'
+
+// ...
+
+const rootUrl = document.currentScript.src.replace(/\/packs.*$/, '')
+
+// Path to the assets folder (should be independent from the current script source path
+// to work correctly in different environments)
+setBasePath(rootUrl + '/packs/js/')

90c8c947be30f5d36fd96993a446c175bfa06a18
Patrick Koers <pkoers75@gmail.com>
2025-04-11 23:26:34 +0200

    changed the confernce model to support only schedule with current conference

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,2 +1,8 @@
 class Conference < ApplicationRecord
+  has_many :schedules, dependent: :destroy, if: -> { current? }
+
+  # Example method to check if the conference is current
+  def current?
+    self.current
+  end
 end

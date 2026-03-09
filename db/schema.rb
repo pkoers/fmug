56360ebb0fe003999106e1da1ac9206e6811abeb
Patrick Koers <pkoers75@gmail.com>
2026-03-09 22:31:09 +0100

    Add admin flag to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_08_191500) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_09_120000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_08_191500) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin", default: false, null: false
     t.bigint "company_id"
     t.datetime "created_at", null: false
     t.string "email", null: false

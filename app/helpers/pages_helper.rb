6804bf3a466676ec5f0a946f7ad53ee66c204a1b
Patrick Koers <pkoers75@gmail.com>
2026-03-22 00:44:32 +0100

    Add markdown-backed content for the landing page about section

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,15 @@
 module PagesHelper
+  def render_markdown_content(path)
+    markdown = Redcarpet::Markdown.new(
+      Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true),
+      autolink: true,
+      fenced_code_blocks: true,
+      tables: true
+    )
+
+    html = markdown.render(File.read(Rails.root.join("content", path)))
+    sanitize(html, tags: %w[p br a em strong code pre ul ol li blockquote h1 h2 h3 h4 h5 h6], attributes: %w[href title])
+  rescue Errno::ENOENT
+    content_tag(:p, "Content file not found: #{path}")
+  end
 end

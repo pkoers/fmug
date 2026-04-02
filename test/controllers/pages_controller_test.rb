db3219d80f274f5c99761d7e3fd7bdbe7d107b11
Patrick Koers <pkoers75@gmail.com>
2026-04-02 21:33:33 +0200

    Revert "Upgrade to Ruby 4.0.2 and make Postgres config portable"
    This reverts commit 944b667a5c3c01aa09c57c4854c712d6bae03889.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -189,7 +189,6 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get privacy_path
 
     assert_response :success
-    assert_includes response.body, "Privacy Statement (DRAFT)"
-    assert_includes response.body, "Who we are"
+    assert_includes response.body, "This is a placeholder privacy statement."
   end
 end

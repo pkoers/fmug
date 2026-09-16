f8ebaad376dfbd2774c5cd22056a9620b9735d01
Patrick Koers <pkoers75@gmail.com>
2026-09-16 21:29:34 +0200

    Add member profile picture management

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require "stringio"
 
 class UserTest < ActiveSupport::TestCase
   test "user is valid without a role" do
@@ -6,6 +7,38 @@ class UserTest < ActiveSupport::TestCase
       email: "member@example.com",
       first_name: "Member",
       last_name: "User"
+    )
+
+    assert user.valid?
+  end
+
+  test "user accepts supported profile picture formats" do
+    %w[image/png image/jpeg image/jpg image/webp].each do |content_type|
+      user = User.new(email: "#{content_type.delete('/')}-member@example.com", first_name: "Member", last_name: "User")
+      user.photo.attach(io: StringIO.new("photo"), filename: "photo", content_type: content_type)
+
+      assert user.valid?, "expected #{content_type} to be accepted"
+    end
+  end
+
+  test "user rejects profile picture larger than 0.5 MB" do
+    user = User.new(email: "member@example.com", first_name: "Member", last_name: "User")
+    user.photo.attach(
+      io: StringIO.new("x" * (User::PHOTO_MAXIMUM_SIZE + 1)),
+      filename: "photo.png",
+      content_type: "image/png"
+    )
+
+    assert_not user.valid?
+    assert_includes user.errors[:photo], "must be 0.5 MB or smaller"
+  end
+
+  test "user accepts profile picture at the maximum size" do
+    user = User.new(email: "member@example.com", first_name: "Member", last_name: "User")
+    user.photo.attach(
+      io: StringIO.new("x" * User::PHOTO_MAXIMUM_SIZE),
+      filename: "photo.png",
+      content_type: "image/png"
     )
 
     assert user.valid?

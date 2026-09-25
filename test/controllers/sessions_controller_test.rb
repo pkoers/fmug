d1233eebf591b542e0e28587dbc3532e0d7cd2ef
Patrick Koers <pkoers75@gmail.com>
2026-09-25 13:56:22 +0200

    Hide Google login button on PROD01

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -18,11 +18,24 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
       info: { email: "googlemember@example.com", first_name: "Google", last_name: "Member" }
     )
 
-    assert_no_difference("User.count") do
-      post "/auth/google_oauth2/callback", env: { "omniauth.auth" => auth }
+    with_environment("APP_URL" => "fmug.eu") do
+      assert_no_difference("User.count") do
+        post "/auth/google_oauth2/callback", env: { "omniauth.auth" => auth }
+      end
     end
 
     assert_equal existing_user.id, session[:user_id]
     assert_equal existing_user, Identity.find_by!(provider: "google_oauth2", uid: "google-member-id").user
+  end
+
+  private
+
+  def with_environment(variables)
+    previous_values = variables.keys.to_h { |key| [ key, ENV[key] ] }
+    variables.each { |key, value| value.nil? ? ENV.delete(key) : ENV[key] = value }
+
+    yield
+  ensure
+    previous_values.each { |key, value| value.nil? ? ENV.delete(key) : ENV[key] = value }
   end
 end
